3eb93fcca22445c9b075f98c5e3d9876838c4db8
DiegoG0477 <diego.14.13.lopez@gmail.com>
2023-11-04 03:12:34 -0600

    crud features are working

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const usersRouter = require('./src/routes/users.route');
 const authRouter = require('./src/routes/auth.route');
 
 app.use(express.json());
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/auth', authRouter);
+app.use('/v1/users', usersRouter);
+app.use('/v1/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log("API escuchando en el puerto " + PORT);
